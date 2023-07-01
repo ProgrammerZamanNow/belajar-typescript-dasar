a195805566366935b911ba9795b25e6fae464ede
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-01 17:08:33 +0700

    Tipe Data Primitif

---
+++ tests/tipe-data.test.ts
@@ -0,0 +1,13 @@
+describe('Data Type', function () {
+    it('should must declare', function () {
+
+        const name: string = "Eko Kurniawan Khannedy";
+        const balance: number = 1000000;
+        const isVip:boolean = true;
+
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+
+    });
+});
