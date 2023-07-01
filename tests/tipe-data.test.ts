b5e6963b211ee432f60e1e0ad6061baf62a763f0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-01 17:18:09 +0700

    Babel TypeScript

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -1,13 +1,17 @@
 describe('Data Type', function () {
     it('should must declare', function () {
 
-        const name: string = "Eko Kurniawan Khannedy";
-        const balance: number = 1000000;
-        const isVip:boolean = true;
+        let name: string = "Eko Kurniawan Khannedy";
+        let balance: number = 1000000;
+        let isVip:boolean = true;
 
         console.info(name);
         console.info(balance);
         console.info(isVip);
+
+        // name = 100;
+        // balance = "eko";
+        // isVip = 100;
 
     });
 });
