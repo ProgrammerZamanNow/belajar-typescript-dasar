f8f1844631043c82be7cf97b17c83c66518d420e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-04 00:37:10 +0700

    Function Interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -15,4 +15,18 @@ describe('Interface', function () {
         console.info(seller);
 
     });
+
+    it('should support function interface', function () {
+
+        interface AddFunction {
+            (value1: number, value2: number): number;
+        }
+
+        const add: AddFunction = (value1: number, value2: number): number => {
+            return value1 + value2;
+        };
+
+        expect(add(2,2)).toBe(4);
+
+    });
 });
