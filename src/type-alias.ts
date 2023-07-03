f863a6e7028feae3f1fb84282fb241297af4a556
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-03 23:52:35 +0700

    Optional Properties

--- src/type-alias.ts
+++ src/type-alias.ts
@@ -3,11 +3,13 @@ export type ID = string | number;
 export type Category = {
     id: ID;
     name: string;
+    description?: string
 }
 
 export type Product = {
     id: ID;
     name: string;
     price: number;
-    category: Category
+    category: Category,
+    description?: string
 }
--- tests/object.test.ts
+++ tests/object.test.ts
@@ -1,7 +1,7 @@
 describe('Object', function () {
     it('should support in typescript', function () {
 
-        const person: {id: string, name: string} = {
+        const person: {id: string, name: string, hobbies?: string[]} = {
             id: "1",
             name: "Eko"
         };
