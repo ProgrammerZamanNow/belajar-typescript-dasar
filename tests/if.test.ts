bc3da599c49734a24b07ea2acb28034c964a0618
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-04 15:54:30 +0700

    Ternary Operator

--- tests/if.test.ts
+++ tests/if.test.ts
@@ -12,4 +12,12 @@ describe('If Statement', function () {
         }
 
     });
+
+    it('should support ternary operator', function () {
+
+        const value = 80;
+        const say = value >= 75 ? "Congratulation" : "Try Again";
+        console.info(say);
+
+    });
 });
