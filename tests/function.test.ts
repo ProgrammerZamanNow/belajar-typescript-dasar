5882480e6c3af1791defea71a1ad77555310a417
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-04 15:20:56 +0700

    Function Parameter

--- tests/function.test.ts
+++ tests/function.test.ts
@@ -14,4 +14,38 @@ describe('Function', function () {
         printHello("Eko");
 
     });
+
+    it('should support default value', function () {
+        function sayHello(name: string = "Guest"): string {
+            return `Hello ${name}`;
+        }
+
+        expect(sayHello()).toBe("Hello Guest");
+        expect(sayHello("Eko")).toBe("Hello Eko");
+    });
+
+    it('should support rest parameter', function () {
+        function sum(...values: number[]): number {
+            let total = 0;
+            for (const value of values) {
+                total += value;
+            }
+            return total;
+        }
+
+        expect(sum(1,2,3,4,5)).toBe(15);
+    });
+
+    it('should support optional parameter', function () {
+        function sayHello(firstName : string, lastName? : string): string {
+            if(lastName){
+                return `Hello ${firstName} ${lastName}`;
+            }else{
+                return `Hello ${firstName}`;
+            }
+        }
+
+        expect(sayHello("Eko")).toBe("Hello Eko");
+        expect(sayHello("Eko", "Kurniawan")).toBe("Hello Eko Kurniawan");
+    });
 });
