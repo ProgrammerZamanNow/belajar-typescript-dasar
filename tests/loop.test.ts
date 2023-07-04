47f788fd75191aad9208a8c82a47733659c0c69f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-04 22:13:30 +0700

    While Loop

--- tests/loop.test.ts
+++ tests/loop.test.ts
@@ -16,4 +16,15 @@ describe('Loop', function () {
         }
 
     });
+
+    it('should support while loop', function () {
+
+        let counter:number = 0;
+
+        while (counter < 10){
+            console.info(counter);
+            counter++;
+        }
+
+    });
 });
