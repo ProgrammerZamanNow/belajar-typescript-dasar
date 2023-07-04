85086149d2c7c5ae583840861d249f493cd7b931
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-07-04 22:19:30 +0700

    Break dan Continue

--- tests/loop.test.ts
+++ tests/loop.test.ts
@@ -38,4 +38,26 @@ describe('Loop', function () {
         }while (counter < 10)
 
     });
+
+    it('should support break and continue', function () {
+
+        let counter : number = 0;
+
+        do {
+
+            counter++;
+
+            if(counter == 10){
+                break;
+            }
+
+            if(counter % 2 == 0){
+                continue;
+            }
+
+            console.info(counter);
+
+        }while (true);
+
+    });
 });
